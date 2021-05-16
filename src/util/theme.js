789333af206c436c13e25f85c8339304b0b7e6dd
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-15 21:33:03 -0500

    Finished Login and Sign up functionality to keep user logged in on refresh if token has not expired. Refactored some code.

---
+++ src/util/theme.js
@@ -0,0 +1,45 @@
+export default {
+    palette: {
+      primary: {
+        light: '#d15353',
+        main: '#c62828',
+        dark: '#8a1c1c',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#ffa533',
+        main: '#ff8f00',
+        dark: '#b26400',
+        contrastText: '#fff'
+      }
+    },
+    spreadThis: {
+      typography: {
+        useNextVariants: true
+      },
+      form: {
+          textAlign: 'center',
+      },
+      image: {
+          margin: '0px auto 20px auto'
+      },
+      pageTitle: {
+          margin: '0px auto 10px auto'
+      },
+      textField: {
+          margin: '5px auto 5px auto'
+      },
+      button: {
+          margin: '15px auto 10px auto',
+          position: 'relative'
+      },
+      customError: {
+          color: 'red',
+          fontSize: '0.8rem',
+          marginTop: 10
+      },
+      progress: {
+          position: 'absolute'
+      }
+    },
+  };
