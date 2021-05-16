6a624492ed11cff6c00553066451d682fac19a09
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-15 20:23:56 -0500

    Added login functionality and styling with connection through axios to the backend API

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link'
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -24,6 +26,7 @@ const styles = {
 
 class Post extends Component {
     render() {
+        dayjs.extend(relativeTime)
             const { classes, post : { body, createdAt, userImage, userHandle, postId, likeCount, commentCount } } = this.props
         return (
             <Card 
@@ -45,7 +48,7 @@ class Post extends Component {
                     <Typography 
                         variant="body2" 
                         color="textSecondary">
-                            {createdAt}
+                            {dayjs(createdAt).fromNow()}
                     </Typography>
                     <Typography 
                         variant="body1">
