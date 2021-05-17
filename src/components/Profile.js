951908c53ed9d8c996aa27997365000717554e42
Voltaire01-W <59457919+Voltaire01-W@users.noreply.github.com>
2021-05-17 10:19:41 -0500

    Added Profile Image upload functionality as well as Logout and Edit Profile Details buttons to the profile section.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,68 +1,43 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import dayjs from 'dayjs';
-// Material UI
+import EditDetails from './EditDetails';
+// MUI Imports
 import Button from '@material-ui/core/Button';
-import { Paper, Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
 import MuiLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+// Icon Imports
+import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import EditIcon from '@material-ui/icons/Edit';
+import KeyboardReturn from '@material-ui/icons/KeyboardReturn';
+import { IconButton, Tooltip } from '@material-ui/core';
+// Redux Imports
+import { connect } from 'react-redux';
+import { logoutUser, uploadImage } from '../redux/actions/userActions';
 
-// Icons
-import { LocationOn } from '@material-ui/icons';
-import { LinkIcon } from '@material-ui/icons/Link';
-import { CalendarToday } from '@material-ui/icons';
-
-const styles = {
-    paper: {
-        padding: 20
-      },
-      profile: {
-        '& .image-wrapper': {
-          textAlign: 'center',
-          position: 'relative',
-          '& button': {
-            position: 'absolute',
-            top: '80%',
-            left: '70%'
-          }
-        },
-        '& .profile-image': {
-          width: 200,
-          height: 200,
-          objectFit: 'cover',
-          maxWidth: '100%',
-          borderRadius: '50%'
-        },
-        '& .profile-details': {
-          textAlign: 'center',
-          '& span, svg': {
-            verticalAlign: 'middle'
-          },
-          '& a': {
-            color: '#00bcd4'
-          }
-        },
-        '& hr': {
-          border: 'none',
-          margin: '0 0 10px 0'
-        },
-        '& svg.button': {
-          '&:hover': {
-            cursor: 'pointer'
-          }
-        }
-      },
-      buttons: {
-        textAlign: 'center',
-        '& a': {
-          margin: '20px 10px'
-        }
-      }
-};
+const styles = (theme) => ({
+  ...theme.spreadThis
+});
 
 class Profile extends Component {
+  handleImageChange = (event) => {
+    const image = event.target.files[0];
+    const formData = new FormData();
+    formData.append('image', image, image.name);
+    this.props.uploadImage(formData);
+  }
+  handleEditPicture = () => {
+    const fileInput = document.getElementById('imageInput');
+    fileInput.click();
+  }
+  handleLogout = () => {
+    this.props.logoutUser();
+  }
     render() {
         const { 
             classes, 
@@ -78,6 +53,12 @@ class Profile extends Component {
                 <div className={classes.profile}>
                     <div className="image-wrapper">
                         <img className="profile-image" src={imageUrl} alt="profile" />
+                        <input type="file" id="imageInput" hidden="hidden" onChange={this.handleImageChange} />
+                        <Tooltip title="Edit profile picture" placement="top">
+                        <IconButton onClick={this.handleEditPicture} className="button">
+                          <EditIcon color="primary"/>
+                        </IconButton>
+                        </Tooltip>
                     </div>
                     <hr />
                     <div className="profile-details">
@@ -105,6 +86,12 @@ class Profile extends Component {
                     <CalendarToday color="primary"/>{' '}
                     <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                     </div>
+                    <Tooltip title="Logout" placement="top">
+                      <IconButton onClick={this.handleLogout}>
+                        <KeyboardReturn color="primary"/>
+                      </IconButton>
+                    </Tooltip>
+                    <EditDetails />
                 </div>
             </Paper>
         ) : (
@@ -131,9 +118,13 @@ const mapStateToProps = (state) => ({
     user: state.user
 });
 
+const mapActionsToProps = { logoutUser, uploadImage };
+
 Profile.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    uploadImage: PropTypes.func.isRequired,
     user: PropTypes.object.isRequired,
     classes: PropTypes.object.isRequired
 }
 
-export default connect(mapStateToProps)(withStyles(styles)(Profile))
+export default connect(mapStateToProps, mapActionsToProps)(withStyles(styles)(Profile))
